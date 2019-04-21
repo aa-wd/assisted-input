579f94af777c138732ea6d73d461c0d8607a48a9
Ahmed Abbas <ahmed.n.abbas@gmail.com>
2019-04-21 15:02:47 +0200

    call function for every assisted input

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log('ts werkt')
+import './main.css';
+
+interface DiacriticsObject {
+  [key: string]: string[];
+};
+
+const addListeners = (element: HTMLInputElement) => {
+
+};
+
+const createAssistedInputs = (diacritics: DiacriticsObject) : void => {
+  const assistedInputs = document.querySelectorAll('input[data-assisted]');
+
+  assistedInputs.forEach((input) => {
+    addListeners(input as HTMLInputElement);
+  });
+};
+
+createAssistedInputs({
+  i: [],
+  a: [],
+});
+
+export default createAssistedInputs;
